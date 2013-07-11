e95e6c1d879bb48b46766e6e27ae861b74374f14
AgoFig <agostinafigueredo@gmail.com>
2013-07-11 11:00:58 -0300

    Update LoginService.java

--- src/main/java/aaarrgh/services/LoginService.java
+++ src/main/java/aaarrgh/services/LoginService.java
@@ -4,6 +4,7 @@ public class LoginService {
 
 	public Boolean authenticate(String username, String password) {
 		return username.equals(password);
+		/* Aca hay que borrar esto de arriba y consultar al dao para comparar los parametros recibidos contra los que se encuentran en la bdd */
 	}
 	
 }
