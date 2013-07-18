902bc10971879c1040478b0290d699c0910e7a24
Paula Montero <pau.montero88@gmail.com>
2013-07-17 23:37:27 -0300

    Arreglos en la conexion y armado de los tweets

--- src/main/java/aaarrgh/services/LoginService.java
+++ src/main/java/aaarrgh/services/LoginService.java
@@ -9,23 +9,18 @@ public class LoginService {
 
 	public Boolean authenticate(String username, String password)
 			throws PersistenceException {
-/*
+
 		UsuarioDao dao = DaoFactory.getUsuarioDao();		
 		Boolean result = false;
 				
 		Usuario correcto = new Usuario();
 		correcto = dao.findByUser(username); // Esto da error hay que ver el dao...
-<<<<<<< HEAD
 		
-		if(correcto.getPassword()== password) { result = true; }
-=======
+		if(password.equals(correcto.getPassword())){
+			result = true; 
+		}
 		
-		if(correcto.getPassword()== password) { result = true; }
-		
-*/
-		
-		
-		return username.equals(password);
+		return result;
 		/*
 		 * Aca hay que borrar esto de arriba y consultar al dao para comparar
 		 * los parametros recibidos contra los que se encuentran en la bdd
