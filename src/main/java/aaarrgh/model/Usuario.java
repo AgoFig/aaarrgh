219308a4a361f461c5d592da2d326a6fbb807879
Paula Montero <pau.montero88@gmail.com>
2013-07-31 12:53:28 -0300

    Con este cambio anda bien seguir / dejar de seguir

--- src/main/java/aaarrgh/model/Usuario.java
+++ src/main/java/aaarrgh/model/Usuario.java
@@ -74,4 +74,68 @@ public class Usuario {
 		this.valido = valido;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((iduser == null) ? 0 : iduser.hashCode());
+		result = prime * result + ((mail == null) ? 0 : mail.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((valido == null) ? 0 : valido.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (iduser == null) {
+			if (other.iduser != null)
+				return false;
+		} else if (!iduser.equals(other.iduser))
+			return false;
+		if (mail == null) {
+			if (other.mail != null)
+				return false;
+		} else if (!mail.equals(other.mail))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (valido == null) {
+			if (other.valido != null)
+				return false;
+		} else if (!valido.equals(other.valido))
+			return false;
+		return true;
+	}
+
+	
 }
